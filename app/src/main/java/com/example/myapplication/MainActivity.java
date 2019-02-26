83a095c4e41cc80110e765a946696017aec0142f
Nihanth Charan Mutluru <nihanthmbc@outlook.com>
2019-02-26 23:24:12 +0530

    Scan Button Enable Disable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,44 +16,54 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private final int REQUEST_IMAGE_CAPTURE = 1, REQUEST_IMAGE_GALLEY = 2;
+    String uploadUri = "http://192.168.100.4/numberScanner";
     private ImageView image;
     private Button camera, gallery, scan;
-    private final int REQUEST_IMAGE_CAPTURE=1,REQUEST_IMAGE_GALLEY=2;
-
+    ProgressDialog progress;
+    String response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = (ImageView)findViewById(R.id.image);
-        camera = (Button)findViewById(R.id.camera);
+        image = (ImageView) findViewById(R.id.image);
+        camera = (Button) findViewById(R.id.camera);
         camera.setOnClickListener(this);
-        gallery = (Button)findViewById(R.id.gallery);
+        gallery = (Button) findViewById(R.id.gallery);
         gallery.setOnClickListener(this);
-        scan = (Button)findViewById(R.id.scanButton);
+        scan = (Button) findViewById(R.id.scanButton);
         scan.setEnabled(false);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.camera:
                 Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(iCamera.resolveActivity(getPackageManager()) != null){
-                    startActivityForResult(iCamera,REQUEST_IMAGE_CAPTURE);
+                if (iCamera.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(iCamera, REQUEST_IMAGE_CAPTURE);
                 }
                 break;
             case R.id.gallery:
-                Intent iGallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent iGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 iGallery.setType("image/'");
-                startActivityForResult(iGallery,REQUEST_IMAGE_GALLEY);
+                startActivityForResult(iGallery, REQUEST_IMAGE_GALLEY);
                 break;
             case R.id.scanButton:
-                Toast.makeText(this, "Scan Button Clicked",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Scan Button Clicked", Toast.LENGTH_SHORT).show();
                 break;
         }
 
@@ -58,37 +72,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK){
-            if(requestCode == REQUEST_IMAGE_CAPTURE){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
                 Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                 image.setImageBitmap(bitmap);
                 scan.setEnabled(true);
-            }else if (requestCode == REQUEST_IMAGE_GALLEY){
+            } else if (requestCode == REQUEST_IMAGE_GALLEY) {
                 Uri uri = data.getData();
                 String x = getPath(uri);
-                Toast.makeText(getApplicationContext(),x,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), x, Toast.LENGTH_LONG).show();
                 Bitmap bitmap = null;
-                try{
-                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                     image.setImageBitmap(bitmap);
                     scan.setEnabled(true);
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
-    public String getPath(Uri uri){
-        if(uri == null) return null;
-        String[] projection = { MediaStore.Images.Media.DATA };
+    public String getPath(Uri uri) {
+        if (uri == null) return null;
+        String[] projection = {MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        if(cursor!=null){
+        if (cursor != null) {
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             return cursor.getString(column_index);
         }
         return uri.getPath();
     }
-
 }
